ff6cd62a038e60d8c3fd007d14b52bb9861eb645
Maccas-H <105921614@student.swin.edu.au>
2025-05-18 18:54:11 +1000

    Removal of unnecessary headers that produces duplication in conjunction with includes/nav

--- cybersecurityspecialist.php
+++ cybersecurityspecialist.php
@@ -14,12 +14,6 @@
 </head>
 
 <body>
-    <!-- ===== HEADER ===== -->
-    <header>
-      <div id="header-top">
-          <img src="styles/images/logo.png" alt="EcruSoft Logo" id="navbar-image">
-          <h1 class="header-title">EcruSoft Solutions</h1>
-      </div>
 
 <!-- Include navbar -->
 <?php include 'includes/navbar.inc.php'; ?>
