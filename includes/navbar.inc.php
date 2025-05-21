3bc95422f6defa9a64b677a3934c985bbc4a3b18
dyljaya <105910395@student.swin.edu.au>
2025-05-21 13:18:29 +1000

    Add Staff button to navbar for manager access

--- includes/navbar.inc.php
+++ includes/navbar.inc.php
@@ -9,6 +9,7 @@
             <li><a href="jobs.php">Jobs</a></li>    
             <li><a href="apply.php">Apply</a></li>
             <li><a href="about.php">About</a></li>
+            <li><a href="manage.php">Staff</a></li>
         </ul>
     </nav>
   </header>
