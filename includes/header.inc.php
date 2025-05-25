f65d4a35ef62315658e75cf9033875ed7ed807e9
Sokhour KIM <104345352@student.swin.edu.au>
2025-05-25 23:29:43 +1000

    Updated header.inc.php

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -1,6 +1,8 @@
 <header>
     <div id="header-top">
-        <img src="styles/images/logo.png" alt="EcruSoft Logo" id="navbar-image">
-        <h1 class="header-title">EcruSoft Solutions</h1>
+        <a href="index.php">
+            <img src="styles/images/logo.png" alt="EcruSoft Logo" id="navbar-image">
+            <h1 class="header-title">EcruSoft Solutions</h1>
+        </a>
     </div>
 </header>
