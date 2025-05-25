27b413f0d179214ac6684849a19b7c1af6df713e
Sokhour KIM <104345352@student.swin.edu.au>
2025-05-25 23:26:30 +1000

    for header process_eoi.php

---
+++ includes/header.inc.php
@@ -0,0 +1,6 @@
+<header>
+    <div id="header-top">
+        <img src="styles/images/logo.png" alt="EcruSoft Logo" id="navbar-image">
+        <h1 class="header-title">EcruSoft Solutions</h1>
+    </div>
+</header>
